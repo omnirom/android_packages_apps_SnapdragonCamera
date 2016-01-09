38b5e1d437bbde3870daa99d68a433d9fd56631a
Björn Fries <fries@oern.de>
2016-01-09 23:03:09 +0100

    Snap: Fix NPE if mCameraDevice == null
    Change-Id: I27ee6f3b6b6bec95661741a0907708e6ebd79752

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2749,8 +2749,7 @@ public class PhotoModule
 
     @Override
     public void stopPreview() {
-        boolean isPreviewing = mCameraDevice.getCamera().previewEnabled();
-        if (mCameraDevice != null && isPreviewing) {
+        if (mCameraDevice != null && mCameraDevice.getCamera().previewEnabled()) {
             if (mCameraState == LONGSHOT) {
                 mCameraDevice.setLongshot(false);
                 mLongshotActive = false;
