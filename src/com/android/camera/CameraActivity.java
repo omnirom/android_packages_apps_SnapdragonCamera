94d9a9073139d8b278fb1b21818ed896fe84b34d
maxwen <max.weninger@gmail.com>
2016-05-25 21:52:46 +0200

    SnapdragonCamera: remove storage menu if no external storage available
    Change-Id: I9807c7f9303f17eb971ff1a82fbd35dfbdbfa529

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -315,6 +315,7 @@ public class CameraActivity extends Activity
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.d(TAG, "SDcard status changed, update storage space");
+            SDCard.instance().scanVolumes();
             updateStorageSpaceAndHint();
         }
     };
