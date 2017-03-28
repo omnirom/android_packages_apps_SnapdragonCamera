fd61c80381eef06da2058c552bf72765a61858fa
maxwen <max.weninger@gmail.com>
2017-03-29 01:31:26 +0200

    SnapdragonCamera: use same shutter sound setting also for video
    Change-Id: I9b583571f72a9529bf67aa1506228f5245f072a0

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -2412,6 +2412,9 @@ public class VideoModule implements CameraModule,
             // startPreview().
             if (mCameraDevice == null) return;
 
+            // TODO there should be a better solution
+            mCameraDevice.enableShutterSound(isShutterSoundOn());
+
             // update menu
             mUI.updateMenuVisibility();
 
@@ -2714,5 +2717,13 @@ public class VideoModule implements CameraModule,
             }
         }
         return -1;
+    }
+
+    private boolean isShutterSoundOn() {
+        String value = mPreferences.getString(CameraSettings.KEY_SHUTTER_SOUND, mActivity.getString(R.string.pref_camera_shuttersound_default));
+        if (value != null && value.equalsIgnoreCase(mActivity.getString(R.string.setting_off_value))) {
+            return false;
+        }
+        return true;
     }
 }
